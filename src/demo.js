d07db5e23c23e8de57e50fc0c1be40f9245c7c80
Maira <makyp002@gmail.com>
2024-06-22 10:12:43 -0500

    Se uso el //@ts-check para evidenciar los errores de Js que no nos dejaba ver a simple vista y se corrigieron

--- src/demo.js
+++ src/demo.js
@@ -1,24 +1,26 @@
-(()=> {
+//@ts-check
+
+(async()=> {
   const myCart = [];
   const products = [];
   const limit = 2;
 
   async function getProducts() {
     const rta = await fetch('http://api.escuelajs.co/api/v1/products', {
-      mehtod: 'GET'
+      method: 'GET'
     });
-    const data = await rta.parseJson();
+    const data = await rta.json();
     products.concat(data);
   }
   function getTotal() {
-    const total = 0;
-    for (const i = 0; i < products.length(); i++) {
+    let total = 0;
+    for (let i = 0; i < products.length; i++) {
       total += products[i].prize;
     }
     return total;
   }
-  function addProduct(index) {
-    if (getTotal <= limit) {
+  function addProducto(index) {
+    if (getTotal => limit) {
       myCart.push(products[index]);
     }
   }
@@ -32,6 +34,6 @@
     name: 'Nicolas',
     lastName: 'Molina'
   }
-  const rta = person +  limit;
+  let rta = "person" +  limit;
   console.log(rta);
 });
